67c00c881e6899bb6b4ae4f7183205c65857f5ee
zhangguanzhang <zhangguanzhang@qq.com>
2018-07-20 15:16:54 +0800

    Update run-vsftpd.sh

--- vsftpd/run-vsftpd.sh
+++ vsftpd/run-vsftpd.sh
@@ -2,15 +2,19 @@
 set -e
 [[ "${DEBUG}" == "true" ]] && set -x
 
+: ${VSFTPD_CONF:=/etc/vsftpd/vsftpd.conf}
+: ${PASV_MAX_PORT:=21110}
+: ${PASV_MIN_PORT:=21100}
+
 sed 's#^#mkdir -p /home/vsftpd/#e;n;D' /etc/vsftpd/virtual_users.txt
 chown -R ftp:ftp /home/vsftpd/ 
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
 # Set passive mode parameters:
 [ -z "$PASV_ADDRESS" ] && export PASV_ADDRESS=$(/sbin/ip route|awk '/default/{print $3}')
-sed -ri '/^pasv_address=/s#=.+#='"$PASV_ADDRESS"'#' ${VSFTPD_CONF:=/etc/vsftpd/vsftpd.conf}
-sed -ri '/^pasv_max_port=/s#=.+#='"${PASV_MAX_PORT:=21110}"'#' $VSFTPD_CONF
-sed -ri '/^pasv_min_port=/s#=.+#='"${PASV_MIN_PORT:=21100}"'#' $VSFTPD_CONF
+sed -ri '/^pasv_address=/s#=.+#='"$PASV_ADDRESS"'#' $VSFTPD_CONF
+sed -ri '/^pasv_max_port=/s#=.+#='"$PASV_MAX_PORT"'#' $VSFTPD_CONF
+sed -ri '/^pasv_min_port=/s#=.+#='"$PASV_MIN_PORT"'#' $VSFTPD_CONF
 
 # stdout server info:
 if [ "$LOG_STDOUT" == false ]; then
